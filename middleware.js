b0b8bffa40dc33c4ee6a54f96fc7aaa966465115
farzamshahid <farzamshahid6@gmail.com>
2024-07-04 16:47:55 +0500

    middleware

--- middleware.js
+++ middleware.js
@@ -1,16 +1,16 @@
-"use client"
 import { NextResponse } from 'next/server'
 import { supabase } from './app/lib/configure'
 
 export async function middleware(request) {
-    const { data: { user }, error } = await supabase.auth.getSession()
+    const { data, error } = await supabase.auth.getSession()
 
-    if (error || !user) {
+    if (error || !data.session) {
+        console.log('data')
         return NextResponse.redirect(new URL('/login', request.url))
     }
     return NextResponse.next()
 }
 
 export const config = {
-    matcher: ['/dashboard'],
+    matcher: ['/', '/dashboard'],
 }
